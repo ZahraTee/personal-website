59e51afe763ad8172571b4bfbe3ad67354cc05e7
Zahra Traboulsi <zahratee@gmail.com>
2024-07-29 16:24:37 +0100

    Add linkable headings in blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,34 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://zahra.dev',
-	integrations: [mdx(), sitemap()],
-	defaultLocale: "en",
-    locales: ["en"],
+  site: "https://zahra.dev",
+  integrations: [mdx(), sitemap()],
+  defaultLocale: "en",
+  locales: ["en"],
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          content: {
+            type: "text",
+            value: "#",
+          },
+          headingProperties: {
+            className: ["anchor"],
+          },
+          properties: {
+            className: ["anchor-link"],
+          },
+        },
+      ],
+    ],
+  },
 });
